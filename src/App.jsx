a8ad5326be9e57d1016ded26e71dfa259915b943
Roya <foroughan.royaa@gmail.com>
2024-01-05 07:57:21 +0330

    first config , route , axios

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {Routes , Route , Navigate} from 'react-router-dom'
+import ProductsPage from './pages/ProductsPage'
+import DetailsPage from './pages/DetailsPage'
+import CheckoutPage from './pages/CheckoutPage'
+import PageNotFound from './pages/404'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>My React Store</h1>
+     <Routes>
+      <Route index element={<Navigate to='/products' replace/>}/>
+      <Route path='/products' element={<ProductsPage/>}/>
+      <Route path='/products/:id' element={<DetailsPage/>}/>
+      <Route path='/checkout' element={<CheckoutPage/>}/>
+      <Route path='/*' element={<PageNotFound/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
