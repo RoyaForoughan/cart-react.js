180f9f7c5d4aeefcd3dcbee66e7a7c82f8525d3d
Roya <foroughan.royaa@gmail.com>
2024-01-08 22:04:29 +0330

    search , category , query string

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,22 +1,52 @@
+import { useSearchParams } from 'react-router-dom'
 import Card from "../components/Card"
 import Loader from "../components/Loader"
 import { useProducts } from "../context/ProductContext"
+import { useEffect, useState } from 'react'
+import { createQueryObject, filterProducts, getInitialQuery, searchProducts } from '../helpers/helpers'
 import styles from './ProductsPage.module.css'
+import SearchBox from '../components/SearchBox'
+import Sidebar from '../components/Sidebar'
 
 function ProductsPage() {
   const products = useProducts()
  
+ 
+
+  const [displayed , setDisplayed] = useState([])
+  const [search , setSearch] = useState('')
+  const [query , setQuery] = useState({})
+  const [searchParams , setSearchParams] = useSearchParams()
+
+  useEffect(() => {
+    setDisplayed(products)
+    setQuery(getInitialQuery(searchParams))
+  } , [products])
+ 
+  useEffect(() => {
+    setSearchParams(query)
+    setSearch(query.search)
+    let finalProducts = searchProducts(products , query.search)
+    finalProducts =  filterProducts(finalProducts , query.category)
+    setDisplayed(finalProducts)
+  } , [query])
+ 
+
+  
 
   return (
+    <>
+      <SearchBox search={search} setSearch={setSearch} setQuery={setQuery}/>
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <Loader/>}
-        {products.map(p => 
+        {!displayed.length && <Loader/>}
+        {displayed.map(p => 
           <Card key={p.id} data={p}/>
           )}
         </div>
-      <div>SideBar</div>
+          <Sidebar setQuery={setQuery}/>
     </div>
+    </>
   )
 }
 
